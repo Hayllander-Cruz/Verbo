82120ce765a92dc417d9780d70ee1f751573d05d
Hayllander-Cruz <cruzhayllander@gmail.com>
2026-05-11 18:20:08 +0100

    Reinício do projeto Verbo: Código Base

---
+++ Principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // Configura o terminal para aceitar caracteres da nossa língua
+    setlocale(LC_ALL, "Portuguese");
+
+    std::cout << "==========================================" << std::endl;
+    std::cout << "   COMPILADOR VERBO v0.1 INICIADO" << std::endl;
+    std::cout << "   Soberania Tecnológica em Português" << std::endl;
+    std::cout << "==========================================" << std::endl;
+
+    return 0;
+}
